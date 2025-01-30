083eb781c9db4ec7d2eeb5fe02ca4ca29a702d73
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-01-30 16:31:45 -0500

    feat: minor change

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ const [myName = 'Friend'] = args
 const hour = new Date().getHours();
 
 if (hour >= 6 && hour < 12) {
-    console.log(`Good morning ${myName}`);
+    console.log(`Good morning ${myName}.`);
 } else if (hour >=12 && hour < 18) {
-    console.log(`Good afternoon ${myName}`);
+    console.log(`Good afternoon ${myName}.`);
 } else if (hour >= 18 && hour < 23) {
-    console.log(`Good evening ${myName}`);
+    console.log(`Good evening ${myName}.`);
 } else {
-    console.log(`Good night ${myName}`);
+    console.log(`Good night ${myName}.`);
 }
