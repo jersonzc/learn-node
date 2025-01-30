0d06f5ff088f50523c9d4b23737a1bb7c656f0ee
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-01-30 16:45:11 -0500

    feat: color answers

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
+import colors from 'colors';
+
 const args = process.argv.slice(2);
 const [myName = 'Friend'] = args
 const hour = new Date().getHours();
 
 if (hour >= 6 && hour < 12) {
-    console.log(`Good morning ${myName}.`);
+    console.log(colors.yellow(`Good morning ${myName}.`));
 } else if (hour >=12 && hour < 18) {
-    console.log(`Good afternoon ${myName}.`);
+    console.log(colors.green(`Good afternoon ${myName}.`));
 } else if (hour >= 18 && hour < 23) {
-    console.log(`Good evening ${myName}.`);
+    console.log(colors.cyan(`Good evening ${myName}.`));
 } else {
-    console.log(`Good night ${myName}.`);
+    console.log(colors.blue(`Good night ${myName}.`));
 }
