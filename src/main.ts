3003cadbcd0744dfcfa2786873347e87b0171806
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-02-04 16:10:38 -0500

    feat: save to file

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
+import fs from 'fs';
+import path from 'path';
 import commandLineArgs, { OptionDefinition } from 'command-line-args';
+
+const filename = 'data.json';
+const filepath = `${path.resolve('.')}/${filename}`;
 
 const params: OptionDefinition[] = [
   {
@@ -24,6 +29,7 @@ const items = [];
 
 if (item) {
   items.push({ item, completed, date });
+  fs.writeFileSync(filepath, JSON.stringify(items, null, 2));
 }
 
 // pnpm run start -i "Run morning" -c -d "today"
