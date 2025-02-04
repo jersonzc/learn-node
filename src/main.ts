146ff91c3f8e1d0ea1b78f220216f550239db355
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-02-04 15:47:54 -0500

    feat: read argument

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import colors from 'colors';
+import commandLineArgs from 'command-line-args';
 
-const args = process.argv.slice(2);
-const [myName = 'Friend'] = args
+const params = [{ name: 'name', alias: 'n', type: String }];
+const options = commandLineArgs(params);
+
+const myName = options.name || 'Friend';
 const hour = new Date().getHours();
 
 if (hour >= 6 && hour < 12) {
-    console.log(colors.yellow(`Good morning ${myName}.`));
-} else if (hour >=12 && hour < 18) {
-    console.log(colors.green(`Good afternoon ${myName}.`));
+  console.log(colors.yellow(`Good morning ${myName}.`));
+} else if (hour >= 12 && hour < 18) {
+  console.log(colors.green(`Good afternoon ${myName}.`));
 } else if (hour >= 18 && hour < 23) {
-    console.log(colors.cyan(`Good evening ${myName}.`));
+  console.log(colors.cyan(`Good evening ${myName}.`));
 } else {
-    console.log(colors.blue(`Good night ${myName}.`));
+  console.log(colors.blue(`Good night ${myName}.`));
 }
