d4f3b01c26f03004e1a7baa557cf67510679ebd3
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-02-04 16:08:50 -0500

    feat: read multiple arguments

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
-import colors from 'colors';
-import commandLineArgs from 'command-line-args';
+import commandLineArgs, { OptionDefinition } from 'command-line-args';
 
-const params = [{ name: 'name', alias: 'n', type: String }];
+const params: OptionDefinition[] = [
+  {
+    name: 'item',
+    alias: 'i',
+    type: String,
+  },
+  {
+    name: 'completed',
+    alias: 'c',
+    type: Boolean,
+  },
+  {
+    name: 'date',
+    alias: 'd',
+    type: String,
+  },
+];
 const options = commandLineArgs(params);
+const { item = '', completed = false, date = '' } = options;
 
-const myName = options.name || 'Friend';
-const hour = new Date().getHours();
+const items = [];
 
-if (hour >= 6 && hour < 12) {
-  console.log(colors.yellow(`Good morning ${myName}.`));
-} else if (hour >= 12 && hour < 18) {
-  console.log(colors.green(`Good afternoon ${myName}.`));
-} else if (hour >= 18 && hour < 23) {
-  console.log(colors.cyan(`Good evening ${myName}.`));
-} else {
-  console.log(colors.blue(`Good night ${myName}.`));
+if (item) {
+  items.push({ item, completed, date });
 }
+
+// pnpm run start -i "Run morning" -c -d "today"
+console.log(items);
